a92c41d90bee0857447f6fe90cae105ddc4a5100
RodolpheBeloncle <rbeloncle@gmail.com>
2023-07-19 21:45:59 +0200

    save init project

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { connectMQTT, subscribeToTopic, unsubscribeFromTopic, disconnectMQTT, publishMessage } from '../pages/mqttService';
+import type { NextPage } from "next";
+
+// const client = mqtt.connect("ws://192.168.1.14:9001");
+// const topic = "mytopic"; // change this to whatever your want
+
+// client.on("connect", () => {
+//   console.log("connected to mqtt broker.");
+
+//   client.subscribe(topic, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//   });
+// });
+
+// const emit = (body: "0" | "1") => {
+//   client.publish(topic, body);
+// };
+
+
+
+
+const Home: NextPage = () => {
+  useEffect(() => {
+    const client = connectMQTT();
+
+    subscribeToTopic('nextopic');
+   
+
+    return () => {
+      unsubscribeFromTopic('nextopic');
+      client && disconnectMQTT()
+    };
+  }, []);
+  return (
+    <>
+      <button onClick={() => publishMessage("nexttopic", "1")}>on</button>
+      <button onClick={() => publishMessage("nextopic", "0")}>off</button>
+    </>
+  );
+};
+
+export default Home;
---
+++ pages/mqttService.js
@@ -0,0 +1,56 @@
+// import fs from 'fs';
+import mqtt from 'mqtt';
+
+const broker_url = process.env.NEXT_PUBLIC_MQTT_BROKER_URL;
+const mqttOptions = {
+  clientId: process.env.NEXT_PUBLIC_CLIENTID,
+  username: process.env.NEXT_PUBLIC_USERNAME,
+  password: process.env.NEXT_PUBLIC_PASSWORD,
+};
+
+let mqttClient;
+console.log(process.env.NEXT_PUBLIC_MQTT_BROKER_URL);
+
+export const connectMQTT = () => {
+  mqttClient = mqtt.connect(
+    process.env.NEXT_PUBLIC_MQTT_BROKER_URL,
+    mqttOptions
+  );
+
+  mqttClient.on('connect', () => {
+    console.log('MQTT connected');
+    // Additional logic after successful connection
+  });
+
+  mqttClient.on('message', (topic, message) => {
+    // Handle incoming MQTT messages
+    console.log(`Received message on topic "${topic}": ${message.toString()}`);
+  });
+
+  return mqttClient;
+};
+
+export const disconnectMQTT = () => {
+  if (mqttClient) {
+    mqttClient.end();
+    console.log('MQTT disconnected');
+  }
+};
+
+export const subscribeToTopic = (topic) => {
+  if (mqttClient) {
+    mqttClient.subscribe(topic);
+  }
+};
+
+export const unsubscribeFromTopic = (topic) => {
+  if (mqttClient) {
+    mqttClient.unsubscribe(topic);
+  }
+};
+
+export const publishMessage = (topic, message) => {
+  if (mqttClient) {
+    mqttClient.publish(topic, message);
+  }
+};
